d340ff2e078505d7cd9a866c40aba23c1d2e6ca2
vovak <vova.kasper.boy@gmail.com>
2026-02-11 16:25:50 +0300

    feat: add content CompanyOverview.tsx

--- app/components/CompanyOverview/CompanyOverview.tsx
+++ app/components/CompanyOverview/CompanyOverview.tsx
@@ -1,9 +1,26 @@
 import styles from './CompanyOverview.module.css';
+import { Switch } from '@/app/components/Switch/Switch';
 export const CompanyOverview = () => {
   return (
     <section className={styles.companyOverview}>
       <div className="container">
-        <div className={styles.block}></div>
+        <div className={styles.block}>
+          {/*Left Part*/}
+          <div className={styles.leftBlock}>
+            <p className={styles.leftBlockTitle}>
+              Agroton is a reliable supplier of agricultural products
+            </p>
+            <p className={styles.leftBlockText}>
+              We cooperate with processors, distributors and retail chains, ensuring stable supplies
+              and high quality standards.
+            </p>
+            <Switch />
+          </div>
+          {/*Right part*/}
+          <div className={styles.rightBlock}>
+            <div className={styles.rightBlockDescription}></div>
+          </div>
+        </div>
       </div>
     </section>
   );
